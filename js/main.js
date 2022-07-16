c375c4e9f8541b1a50937d673d308a6fee4aa95e
Sergey <sergey@example.com>
2022-07-16 22:28:37 +0300

    make second part app

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// part 1
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 const personalMovieDB = {
@@ -8,12 +10,31 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-b = prompt('На сколько оцените его?'),
-c = prompt('Один из последних просмотренных фильмов?'),
-d = prompt('На сколько оцените его?');
+for(i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?'),
+  b = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a != null && b != null && a != '' &&  b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (10 < personalMovieDB.count <= 30 ) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  alert('Вы киноман');
+} else {
+  alert('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
+
+
+
